28a447c9c8e3ff617822c39357033879e80e4827
Grandon Smith <grandon.smith@yahoo.com>
2020-12-07 10:10:04 -0500

    movie query params work as intended, there's probably a more efficient way to use the if statements. maybe a switch would be better.

--- server.js
+++ server.js
@@ -22,9 +22,42 @@ app.use(function validateBearerToken(req, res, next) {
 });
 
 function handleGetMovie(req, res) {
-    const { genre, country, vote } = req.query
-    let movieList = MOVIES
-    res.send("incoming movies")
+    const { genre, country, vote } = req.query;
+    let movieList;
+    if(!genre && !country && !vote) {
+        movieList = MOVIES
+    }
+    if(genre) {
+        movieList = MOVIES.filter(movie =>
+             movie.genre.toLowerCase().includes(genre.toLowerCase()))
+    }
+    if(country) {
+        movieList = MOVIES.filter(movie =>
+             movie.country.toLowerCase().includes(country.toLowerCase()))
+    }
+    if(vote) {
+        movieList = MOVIES.filter(movie =>
+             movie.avg_vote >= vote)
+    }
+    if(genre && vote) {
+        let list = MOVIES.filter(movie =>
+             movie.genre.toLowerCase().includes(genre.toLowerCase()))
+        movieList = list.filter(movie => movie.avg_vote >= vote)
+    }
+    if(country && vote) {
+        let list = MOVIES.filter(movie =>
+             movie.country.toLowerCase().includes(country.toLowerCase()))
+        movieList = list.filter(movie => movie.avg_vote >= vote)
+    }
+    if(genre && country && vote) {
+        let list = MOVIES.filter(movie =>
+             movie.country.toLowerCase().includes(country.toLowerCase()))
+        let list2 = list.filter(movie =>
+             movie.genre.toLowerCase().includes(genre.toLowerCase()))
+        movieList = list2.filter(movie => movie.avg_vote >= vote)
+    }
+    // let movieList = MOVIES
+    res.send(movieList)
 }
 
 app.get('/movie', handleGetMovie)
